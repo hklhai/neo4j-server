9f96b1959108acd60e02d0f9aa66519249477042
hklhai <hkhai@outlook.com>
2018-07-13 14:15:55 +0800

    modify

--- export_es/export_es_data.py
+++ export_es/export_es_data.py
@@ -1,87 +1,28 @@
 # -*- coding: utf-8 -*-
-
-"""
-
-/**
- * curl -H "Content-Type: application/x-ndjson" -XPOST "spark3:9200/film_data/film/_bulk?pretty" --data-binary @film_data.json
- * <p>
- * 如果提示 Result window is too large
- */
-@Override
-public void export() {
-    SearchResponse response = client.prepareSearch("market_book")
-            .setTypes("book").setQuery(QueryBuilders.matchAllQuery())
-            .execute().actionGet();
-    SearchHits resultHits = response.getHits();
-
-    Long totalHits = resultHits.totalHits;
-    response = client.prepareSearch("market_book")
-            .setTypes("book").setQuery(QueryBuilders.matchAllQuery()).setFrom(0).setSize(totalHits.intValue())
-            .execute().actionGet();
-    resultHits = response.getHits();
-
-    StringBuilder stringBuilder = new StringBuilder(128214);
-    for (int i = 0; i < resultHits.getHits().length; i++) {
-        String jsonStr = resultHits.getHits()[i].getSourceAsString();
-
-        String index = "{\"index\":{\"_index\":\"market_book2\",\"_id\":" + i + "}}\n";
-        stringBuilder.append(index);
-        stringBuilder.append(jsonStr).append("\n");
-    }
-    String fileName = "d:\\film_data.json";
-    FileUtils.writeStrToFile(stringBuilder.toString(), fileName);
-}
-"""
-import json
-import os
-import time
-import urllib
+from elasticsearch import Elasticsearch
 
 
-class ExportElasticSearchData:
-    size = 10000
+def export():
+    es = Elasticsearch(["spark3:9200"])
+    index_name = "search_text"
+    type_name = "text"
+    target_index_name = "search_text"
+    file_name = "D:\search_text.json"
 
-    def __init__(self, url, index, type):
-        self.url = url + "/" + index + "/" + type + "/_search"
-        self.index = index
-        self.type = type
+    count = es.count(index=index_name, doc_type=type_name)['count']
+    body = {"size": count}
+    data = es.search(index=index_name, doc_type=type_name, body=body)['hits']['hits']
 
-    def export_data(self):
-        print("export data begin...")
-        begin = time.time()
-        try:
-            os.remove(self.index + "_" + self.type + ".json")
-        except Exception as e:
-            os.mknod(self.index + "_" + self.type + ".json")
-        msg = urllib.request.urlopen(self.url).read()
-        print(msg)
-        obj = json.loads(msg)
-        num = obj["hits"]["total"]
-        start = 0
-        end = num / self.size + 1
-        while start < end:
-            msg = urllib.request.urlopen(
-                self.url + "?from=" + str(start * self.size) + "&size=" + str(self.size)).read()
-            self.write_file(msg)
-            start = start + 1
-        print("export data end!!!\n\t total consuming time:" + str(time.time() - begin) + "s")
-
-    def write_file(self, msg):
-        obj = json.loads(msg)
-        vals = obj["hits"]["hits"]
-        try:
-            f = open(self.index + "_" + self.type + ".json", "a")
-            for val in vals:
-                a = json.dumps(val["_source"], ensure_ascii=False)
-                f.write(a + "\n")
-        finally:
-            f.flush()
-            f.close()
+    tmp = ""
+    for i in range(len(data) - 1):
+        index = "{\"index\":{\"_index\":\"" + target_index_name + "\",\"_id\":" + str(i) + "}}\n"
+        tmp += index
+        tmp += str(data[i]['_source'])
+        tmp += "\n"
 
 
 if __name__ == '__main__':
     """
-     * curl -H "Content-Type: application/x-ndjson" -XPOST "spark3:9200/film_data/film/_bulk?pretty" --data-binary @film_data.json
+    curl -H "Content-Type: application/x-ndjson" -XPOST "spark3:9200/film_data/film/_bulk?pretty" --data-binary @film_data.json
     """
-    # ExportElasticSearchData("http://spark3:9200", "chapter_data", "chapter").export_data()
-    ExportElasticSearchData("http://spark3:9200", "news_data", "news").export_data()
+    export()
